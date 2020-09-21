2b91b628e3587b6a6f277751537f9b3ae0017bc3
Anthony <anthony.r.young@gmail.com>
2020-09-20 22:13:21 -0600

    Updating test voxel generations

--- Source/VoxelMesher/ue-src/TestMesh.cpp
+++ Source/VoxelMesher/ue-src/TestMesh.cpp
@@ -4,14 +4,50 @@
 
 namespace
 {
-	static std::vector<int32_t> big_block(size_t size)
-	{
-		std::vector<int32_t> result{};
-		result.resize(size * size * size, 1);
+template <typename Func>
+static std::tuple<std::vector<int32_t>, tc::vector3i> make_voxels(tc::vector3i l, tc::vector3i h,
+								  Func &&f)
+{
+	tc::vector3i d{ h[0] - l[0], h[1] - l[1], h[2] - l[2] };
+	std::vector<int32_t> v(d[0] * d[1] * d[2]);
+	int32_t n = 0;
 
-		return result;
+	for (auto k = l[2]; k < h[2]; ++k) {
+		for (auto j = l[1]; j < h[1]; ++j) {
+			for (auto i = l[0]; i < h[0]; ++i, ++n) {
+				v[n] = f(i, k, j);
+			}
+		}
 	}
+	return std::make_tuple( v, d );
 }
+
+static std::tuple<std::vector<int32_t>, tc::vector3i> big_block(size_t size)
+{
+	int32_t s{ static_cast<int32_t>(size) };
+	std::vector<int32_t> result{};
+	result.resize(size * size * size, 1);
+
+	return std::make_tuple(result, tc::vector3i{ s, s, s });
+}
+
+static std::tuple<std::vector<int32_t>, tc::vector3i> hill()
+{
+	return make_voxels(tc::vector3i{ -16, 0, -16 }, tc::vector3i{ 16, 16, 16 },
+			   [](auto i, auto j, auto k) {
+				   return j <= 16 * exp(-(i * i + k * k) / 64.0);
+			   });
+}
+
+static std::tuple<std::vector<int32_t>, tc::vector3i> valley()
+{
+	return make_voxels(tc::vector3i{ 0, 0, 0 }, tc::vector3i{ 32, 32, 32 },
+			   [](auto i, auto j, auto k) {
+				   return j <= (i*i + k*k) * 31.0 / (32*32*2) + 1;
+			   });
+}
+
+} // namespace
 
 ATestMesh::ATestMesh()
 {
@@ -21,8 +57,8 @@ ATestMesh::ATestMesh()
 	procedural_mesh = CreateDefaultSubobject<UProceduralMeshComponent>(TEXT("ProceduralMesh"));
 	procedural_mesh->bCastDynamicShadow = false;
 	procedural_mesh->bUseAsyncCooking = true;
-	procedural_mesh->AttachToComponent(
-		RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	procedural_mesh->AttachToComponent(RootComponent,
+					   FAttachmentTransformRules::KeepRelativeTransform);
 }
 
 void ATestMesh::BeginPlay()
@@ -30,9 +66,10 @@ void ATestMesh::BeginPlay()
 	Super::BeginPlay();
 
 	tc::simple_voxel_mesher mesher{};
-	static constexpr size_t size = 1;
-	auto block = big_block(size);
-	auto result = mesher.eval(std::begin(block), std::end(block), size, size, size);
+	static constexpr size_t size = 10;
+	auto &&[block, dimensions] = valley(); //big_block(size);
+	auto result = mesher.eval(std::begin(block), std::end(block), dimensions.x, dimensions.y,
+				  dimensions.z);
 
 	TArray<FVector> vertices;
 	TArray<int32> triangles;
@@ -46,61 +83,41 @@ void ATestMesh::BeginPlay()
 	uvs.Reserve(result.vertices.size());
 	triangles.Reserve(result.quads.size() * 6);
 
-	auto& verts = result.vertices;
-	for (auto& quad : result.quads)
-	{
+	auto &verts = result.vertices;
+	for (auto &quad : result.quads) {
 		auto first = vertices.Num();
 		auto uv = std::begin(quad.uv);
-		for (auto q : quad)
-		{
-			auto& vert = verts[q];
-			vertices.Emplace(
-			FVector{
-				static_cast<float>(vert.x) * 100.0f - 50.0f,
-				static_cast<float>(vert.y) * 100.0f - 50.0f,
-				static_cast<float>(vert.z) * 100.0f - 50.0f
-			}
-			);
-			auto& n = quad.normal;
-			normals.Emplace(FVector{
-				static_cast<float>(n.x),
-				static_cast<float>(n.y),
-				static_cast<float>(n.z)
-			});
-			uvs.Emplace(FVector2D{
-				static_cast<float>(uv->x),
-				static_cast<float>(uv->y)
-			});
+		for (auto q : quad) {
+			auto &vert = verts[q];
+			vertices.Emplace(FVector{ static_cast<float>(vert.x) * 100.0f - 50.0f,
+						  static_cast<float>(vert.y) * 100.0f - 50.0f,
+						  static_cast<float>(vert.z) * 100.0f - 50.0f });
+			auto &n = quad.normal;
+			normals.Emplace(FVector{ static_cast<float>(n.x), static_cast<float>(n.y),
+						 static_cast<float>(n.z) });
+			uvs.Emplace(
+				FVector2D{ static_cast<float>(uv->x), static_cast<float>(uv->y) });
 			++uv;
 		}
 
 		// use offset from "first" for the index of triangles.
 		for (auto i = 0; i < 3; ++i) {
-			auto j = (3 - i)  % 3;
+			auto j = (3 - i) % 3;
 			triangles.Emplace(first + j);
 		}
 
 		for (auto i = 0; i < 3; ++i) {
-			auto j = (4 - i)  % 4;
+			auto j = (4 - i) % 4;
 			triangles.Emplace(first + j);
 		}
 	}
 
-	// uvs.Init(FVector2D(0.0f, 0.0f), vertices.Num());
 	// vertexColors.Init(FLinearColor(0.0f, 0.0f, 0.0f, 1.0f), vertices.Num());
 	// tangents.Init(FProcMeshTangent(1.0f, 0.0f, 0.0f), vertices.Num());
 
 	//Function that creates mesh section
-	procedural_mesh->CreateMeshSection_LinearColor(
-		0,
-		vertices,
-		triangles,
-		normals,
-		uvs,
-		vertexColors,
-		tangents,
-		false
-		);
+	procedural_mesh->CreateMeshSection_LinearColor(0, vertices, triangles, normals, uvs,
+						       vertexColors, tangents, false);
 
 	procedural_mesh->SetMaterial(0, material);
 }
